9db78e0e3bdd520346afedf43dd55edf90c38935
ATUL KUMAR <atul.0298@gmail.com>
2025-11-27 16:48:15 +0100

    Update Volcano_Plot_EV_tMCAo.R

--- R_Code/Volcano_Plot_EV_tMCAo.R
+++ R_Code/Volcano_Plot_EV_tMCAo.R
@@ -18,11 +18,11 @@ df <- fread ("~/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Project
 p <- ggplot (data = df, aes (x = Log2FC, y = -log10(p_Value), col = Regulation, label = Label))+
   geom_point ()+ geom_text_repel(max.overlaps = Inf, show.legend  = F)
 p <- p + geom_hline (aes(yintercept=-log10(0.05), linetype = "p-value 0.05", col="black")) +
-  geom_hline (aes (yintercept=-log10(0.0000548446), linetype = "FDR p-value 0.05", col="#D55E00")) +
+  geom_hline (aes (yintercept=-log10(0.00004277159), linetype = "FDR p-value 0.05", col="#D55E00")) +
   scale_linetype_manual(name = "p-value cut off", values = c(2, 2), 
                         guide = guide_legend(override.aes = list(color = c("black", "#D55E00")))) ##0.00518617021 (with No Dynamics)
 p <- p + scale_color_manual(values=cbbPalette, limits = force) + theme_light()
-p <- p + scale_x_continuous(breaks = round(seq(-5, 7, by = 1),1))
+p <- p + scale_x_continuous(breaks = round(seq(-4, 6, by = 1),1))
 p <- p + scale_y_continuous(breaks = round (seq (0, 10, by = 1), 1))
 p <- p + xlab (expression (log[2]~"Fold Change")) + labs (color = "Regulation") + ylab (expression (-log[10]~(P)))
 p <- p +
