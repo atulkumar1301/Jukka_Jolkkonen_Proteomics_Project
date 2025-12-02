8f9edd480dd5c424fcea42fbb3cfea37ecfb645e
ATUL KUMAR <atul.0298@gmail.com>
2025-12-02 14:13:57 +0200

    Update Volcano_Plot_Total_GI.R

--- R_Code/Volcano_Plot_Total_GI.R
+++ R_Code/Volcano_Plot_Total_GI.R
@@ -18,15 +18,15 @@ df <- fread ("~/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Project
 p <- ggplot (data = df, aes (x = Log2FC, y = -log10(p_Value), col = Regulation, label = Label))+
   geom_point ()+ geom_text_repel(max.overlaps = Inf, show.legend  = F)
 p <- p + geom_hline (aes(yintercept=-log10(0.05), linetype = "p-value 0.05", col="black")) +
-  geom_hline (aes (yintercept=-log10(0.0000249128), linetype = "FDR p-value 0.05", col="#D55E00")) +
+  geom_hline (aes (yintercept=-log10(0.00025614754), linetype = "FDR p-value 0.25", col="#D55E00")) +
   scale_linetype_manual(name = "p-value cut off", values = c(2, 2), 
                         guide = guide_legend(override.aes = list(color = c("black", "#D55E00")))) ##0.00518617021 (with No Dynamics)
 p <- p + scale_color_manual(values=cbbPalette, limits = force) + theme_light()
-p <- p + scale_x_continuous(breaks = round(seq(-2, 5, by = 1),1))
+p <- p + scale_x_continuous(breaks = round(seq(-2, 3, by = 1),1))
 p <- p + scale_y_continuous(breaks = round (seq (0, 10, by = 1), 1))
 p <- p + xlab (expression (log[2]~"Fold Change")) + labs (color = "Regulation") + ylab (expression (-log[10]~(P)))
 p <- p +
-  theme(legend.position="left",
+  theme(legend.position="bottom",
         plot.title = element_text(family = "serif", size=18, face = "bold", hjust = 0.5),
         axis.title.x = element_text(family = "serif", size=16),
         axis.title.y = element_text(family = "serif", size=16),
